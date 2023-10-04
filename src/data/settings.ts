57538f0ad17127f6380f986a1ce0ca687daea37d
Andrew Aguirre <andrewa96@ymail.com>
2023-10-03 19:04:59 -0700

    build initial layout

---
+++ src/data/settings.ts
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    title: "Youtube",
+    description: "Watch =>",
+    link: "https://www.youtube.com/channel/UC3t-40u8HVu1LeRs7NcYVog",
+  },
+  {
+    title: "Spotify",
+    description: "Listen =>",
+    link: "https://open.spotify.com/show/1dL1MEG9fH6wAzICQyl6Io",
+  },
+  {
+    title: "Instagram",
+    description: "Follow =>",
+    link: "https://www.instagram.com/ablackkatsvoice",
+  },
+  {
+    title: "Facebook",
+    description: "Follow =>",
+    link: "https://www.facebook.com/ablackkatsvoice",
+  },
+];
