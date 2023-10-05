261451be43475582471ba3aaf74afc9c59a4dfe8
Andrew Aguirre <andrewa96@ymail.com>
2023-10-04 18:59:41 -0700

    2nd pass

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,22 +1,54 @@
+export const siteData = {
+  title: "A Black Kat's Voice",
+  description:
+    "Hey my lovely people!! Welcome to the first blog page for my podcast, A Black Kat’s Voice😊😏🎙",
+  siteName: "A Black Kat's Voice",
+  image: {
+    url: "/images/og-twitter-card.jpg",
+    alt: "A black cat",
+  },
+  twitter: {
+    creator: "@ablackkatsvoice",
+  },
+};
+
 export const links = [
   {
     title: "Youtube",
-    description: "Watch =>",
+    icon: "logos:youtube",
+    height: "26px",
+    description: "Watch",
     link: "https://www.youtube.com/channel/UC3t-40u8HVu1LeRs7NcYVog",
   },
   {
     title: "Spotify",
-    description: "Listen =>",
+    icon: "logos:spotify",
+    height: "34px",
+    description: "Listen",
     link: "https://open.spotify.com/show/1dL1MEG9fH6wAzICQyl6Io",
   },
   {
     title: "Instagram",
-    description: "Follow =>",
+    icon: "logos:instagram",
+    height: "30px",
+    description: "Follow",
     link: "https://www.instagram.com/ablackkatsvoice",
   },
   {
     title: "Facebook",
-    description: "Follow =>",
+    icon: "logos:facebook",
+    height: "34px",
+    description: "Follow",
     link: "https://www.facebook.com/ablackkatsvoice",
   },
+  {
+    title: "TikTok",
+    icon: "logos:tiktok",
+    height: "34px",
+    description: "Follow",
+    link: "https://www.tiktok.com/@ablackkatsvoice",
+  },
 ];
+
+export const amazonLink =
+  "https://www.amazon.com/Black-Kats-Voice-Broken-Heart/dp/B0BVT7HJ9W";
